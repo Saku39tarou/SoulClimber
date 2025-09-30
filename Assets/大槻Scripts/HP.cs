230e05d48c5c267d1845458f8b29bbedf29d93d4
Saku39tarou <hmj2422020@stu.o-hara.ac.jp>
2025-09-30 13:56:43 +0900

    HPバーのプレファブ化、HPスクリプトをつけたPlayerプレファブ更新

--- Assets/大槻Scripts/HP.cs
+++ Assets/大槻Scripts/HP.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.Events;
 
 public class HP : MonoBehaviour
 {
 	[SerializeField] UnityEvent onDieCallback = new UnityEvent();
-	[SerializeField] int life = 100;
+	[SerializeField] int life=100;	
 	[SerializeField] Slider HpBar;
 
     void Start()
     {
+	
         if(HpBar != null)
 		{
 			HpBar.value = life;
@@ -23,6 +25,9 @@ public class HP : MonoBehaviour
 		if (life <= 0) return;
 
 		life -= damage;
+
+
+
 		if(HpBar != null)
 		{
 			HpBar.value = life;
@@ -40,6 +45,11 @@ public class HP : MonoBehaviour
 			HpBar.value += 10;
 			Destroy(collision.gameObject);
 		}
+	}
+
+	public void TakeDamage(int damage)
+	{
+		HpBar.value -= damage;
 	}
 	void OnDie()
 	{
