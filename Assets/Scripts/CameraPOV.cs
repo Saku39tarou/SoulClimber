cb718a6e7b8190022df49ab20629999565256e69
sy_work <hmj2422019@stu.o-hara.ac.jp>
2025-10-06 12:20:07 +0900

    ステージの作成途中、自分のシーンにテストプレイヤーを追加

---
+++ Assets/Scripts/CameraPOV.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraPOV : MonoBehaviour
+{
+	[SerializeField] GameObject cam;
+	[SerializeField] InputActionReference lookAction;
+	[SerializeField] float mouseSensitivity = 0.1f;
+	[SerializeField] float gamepadSensitivity = 3.0f;
+
+	[SerializeField] float minX = -90f;
+	[SerializeField] float maxX = 90f;
+
+	Quaternion cameraRot, characterRot;
+
+	bool cursorLock = true;
+
+	void Start()
+	{
+		cameraRot = cam.transform.localRotation;
+		characterRot = transform.localRotation;
+		lookAction.action.Enable();
+	}
+
+	void LateUpdate()
+	{
+		HandleLook();
+		UpdateCursorLock();	//カーソルの表示非表示
+	}
+
+	void HandleLook()
+	{
+		Vector2 lookInput = lookAction.action.ReadValue<Vector2>();
+
+		// 入力デバイスを判定
+		float currentSensitivity = mouseSensitivity;
+
+		if (lookAction.action.activeControl != null &&
+			lookAction.action.activeControl.device is Gamepad)
+		{
+			currentSensitivity = gamepadSensitivity;
+		}
+
+		float xRot = lookInput.x * currentSensitivity;
+		float yRot = lookInput.y * currentSensitivity;
+
+		cameraRot *= Quaternion.Euler(-yRot, 0, 0);
+		characterRot *= Quaternion.Euler(0, xRot, 0);
+
+		cameraRot = ClampRotation(cameraRot);
+
+		cam.transform.localRotation = cameraRot;
+		transform.localRotation = characterRot;
+	}
+	//ゲーム画面をクリックするとカーソルを非表示にする
+	public void UpdateCursorLock()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape)) cursorLock = false;
+		else if (Input.GetMouseButton(0)) cursorLock = true;
+
+		Cursor.lockState = cursorLock ? CursorLockMode.Locked : CursorLockMode.None;
+	}
+
+	//角度制限関数の作成
+	public Quaternion ClampRotation(Quaternion q)
+	{
+		q.x /= q.w;
+		q.y /= q.w;
+		q.z /= q.w;
+		q.w = 1f;
+
+		float angleX = Mathf.Atan(q.x) * Mathf.Rad2Deg * 2f;
+		angleX = Mathf.Clamp(angleX, minX, maxX);
+		q.x = Mathf.Tan(angleX * Mathf.Deg2Rad * 0.5f);
+
+		return q;
+	}
+}
