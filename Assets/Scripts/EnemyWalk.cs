e227dffd863ed770d06ba809797bf711a26f4fc0
hmj2422030-admin <hmj2422030@stu.o-hara.ac.jp>
2025-11-11 14:37:15 +0900

    敵の視線完成:だけどあわせるのはまだ

--- Assets/Scripts/EnemyWalk.cs
+++ Assets/Scripts/EnemyWalk.cs
@@ -5,20 +5,30 @@ using UnityEngine.AI;
 
 public class EnemyWalk : MonoBehaviour
 {
+	[SerializeField] GameObject player; 
 	[SerializeField] Transform[] goals;
+
 	[SerializeField] int destNum = 0;
+	[SerializeField] float distance;
+	[SerializeField] float angle = 45f;
+
+	bool attack;
+
 	private NavMeshAgent agent;
 	// Start is called before the first frame update
 	void Start()
     {
 		agent = GetComponent<NavMeshAgent>();
 		agent.destination = goals[destNum].position;
+		player = GameObject.Find("Player");
+		attack = false;
 	}
 
 	void nextGoal()
 	{
 		destNum += 1;
-		if (destNum == 3)
+
+		if (destNum == 4)
 		{
 			destNum = 0;
 		}
@@ -28,13 +38,57 @@ public class EnemyWalk : MonoBehaviour
 	Debug.Log(destNum);
 	}
 
+	void EnemyAttack()
+	{
+		attack = true;
+	}
+
 	// Update is called once per frame
 	void Update()
 	{
-		Debug.Log(agent.destination);
-		if (agent.remainingDistance < 0.5f)
+		if (agent.remainingDistance < 1)
 		{
 			nextGoal();
+		}
+
+		if (destNum == 3)
+		{
+			EnemyAttack();
+		}
+
+		if(attack)
+		{
+			Debug.Log("OK");
+		}
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player") //Ž‹ŠE‚Ì”ÍˆÍ“à‚Ì“–‚½‚è”»’è
+		{
+			//Ž‹ŠE‚ÌŠp“x“à‚ÉŽû‚Ü‚Á‚Ä‚¢‚é‚©
+			Vector3 posDelta = other.transform.position - this.transform.position;
+			float target_angle = Vector3.Angle(this.transform.forward, posDelta);
+
+			if (target_angle < angle) //target_angle‚ªangle‚ÉŽû‚Ü‚Á‚Ä‚¢‚é‚©‚Ç‚¤‚©
+			{
+				if (Physics.Raycast(this.transform.position, posDelta, out RaycastHit hit)) //Ray‚ðŽg—p‚µ‚Ätarget‚É“–‚½‚Á‚Ä‚¢‚é‚©”»•Ê
+				{
+					if (hit.collider == other)
+					{
+						destNum = 3;
+					}
+				}
+			}
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject.tag == "Player") //Ž‹ŠE‚Ì”ÍˆÍ“à‚Ì“–‚½‚è”»’è
+		{
+			attack = false;
+			destNum = 0;
 		}
 	}
 }
