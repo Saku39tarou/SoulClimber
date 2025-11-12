d83fa9d7b7d2d625407d6ff99c68f59781475007
24mon <hmj2422002@stu.o-hara.ac.jp>
2025-11-12 12:10:18 +0900

    UI実装完了

--- Assets/辻本さん/SkySystem.cs
+++ Assets/辻本さん/SkySystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.IO.LowLevel.Unsafe;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -15,12 +16,10 @@ public class SkySystem : MonoBehaviour
 	private float alpha;
 	[SerializeField]
 	TextMeshProUGUI skySituation;
-	[SerializeField]
-	GameObject fadeSwich;
+	
 
-
-	[SerializeField] bool fadeIn;
-	[SerializeField] bool fadeOut;
+	//[SerializeField] bool fadeIn;
+	//[SerializeField] bool fadeOut;
 
 	private float nextWaitTime;
 	public int dayCount = 1;
@@ -32,11 +31,20 @@ public class SkySystem : MonoBehaviour
 	}
 	[SerializeField] Sky sky;
 
+	enum FadeState
+	{
+		FadeStart,
+		FadeIn,
+		FadeOut,
+		FadeEnd,
+	}
+	[SerializeField] FadeState fadeState = FadeState.FadeStart;
+
 
 	void Start()
 	{
-		fadeIn = true;
-		fadeOut = false;
+		//fadeIn = true;
+		//fadeOut = false;
 		skySituation.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
 		SkyNames = new string[] { "朝", "もうすぐ日が沈む....", "夜", "もうすぐ夜が明ける..." };
 		skySituation.text = "";
@@ -50,35 +58,118 @@ public class SkySystem : MonoBehaviour
 		//1日のスピードを調節する
 		sunPos += Time.deltaTime;
 
+
+		if (sunPos >= 159 && sunPos < 160)
+		{
+			fadeState = FadeState.FadeStart;
+		}
+
 		if (sunPos >= 160 && sunPos <= 170)
 		{
-			skySituation.text = SkyNames[1];
-			if (fadeIn) FadeIn();
-			if (fadeOut) FadeOut();
+			//if (fadeIn) FadeIn();
+			//if (fadeOut) FadeOut();
+			switch (fadeState)
+			{
+
+				case FadeState.FadeStart:
+					fadeState = FadeState.FadeIn;
+					break;
+
+				case FadeState.FadeIn:
+					skySituation.text = SkyNames[1];
+					if (fadeState == FadeState.FadeIn) FadeIn();
+					break;
+				case FadeState.FadeOut:
+					if (fadeState == FadeState.FadeOut) FadeOut();
+					break;
+			}
+		}
+
+		if (sunPos >= 179 && sunPos < 180)
+		{
+			fadeState = FadeState.FadeStart;
 		}
 		if (sunPos >= 180 && sunPos <= 190)
 		{
+			switch (fadeState)
+			{
+				case FadeState.FadeStart:
+					fadeState = FadeState.FadeIn;
+					break;
 
-			skySituation.text = SkyNames[2];
-			if (fadeIn) FadeIn();
-			if (fadeOut) FadeOut();
+				//case FadeState.FadeStart:
+				//	fadeState = FadeState.FadeIn;
+				//	break;
+
+				case FadeState.FadeIn:
+					skySituation.text = SkyNames[2];
+					if (fadeState == FadeState.FadeIn) FadeIn();
+					break;
+				case FadeState.FadeOut:
+					if (fadeState == FadeState.FadeOut) FadeOut();
+					break;
+			}
+			//if (fadeIn) FadeIn();
+			//if (fadeOut) FadeOut();
 			sky = Sky.Night;
 		}
+
+		if (sunPos >= 329 && sunPos < 329.5)
+		{
+			fadeState = FadeState.FadeStart;
+		}
+
 		if (sunPos >= 330 && sunPos <= 340)
 		{
+			switch (fadeState)
+			{
 
-			skySituation.text = SkyNames[3];
-			if (fadeIn) FadeIn();
-			if (fadeOut) FadeOut();
+				case FadeState.FadeStart:
+					fadeState = FadeState.FadeIn;
+					break;
+
+				case FadeState.FadeIn:
+					skySituation.text = SkyNames[3];
+					if (fadeState == FadeState.FadeIn) FadeIn();
+					break;
+				case FadeState.FadeOut:
+					if (fadeState == FadeState.FadeOut) FadeOut();
+					break;
+			}
+			//if (fadeIn) FadeIn();
+			//if (fadeOut) FadeOut();
 		}
+
+		if (sunPos >= 359 && sunPos < 359.5)
+		{
+			fadeState = FadeState.FadeStart;
+		}
+
 		if (sunPos >= 360)
 		{
-
-			skySituation.text = SkyNames[0];
-			if (fadeIn) FadeIn();
-			if (fadeOut) FadeOut();
+			//if (fadeIn) FadeIn();
+			//if (fadeOut) FadeOut();
 			sunPos = 0;
 			sky = Sky.Day;
+		}
+
+		if(sunPos >= 0 && sunPos <= 10)
+		{
+			switch (fadeState)
+			{
+
+				case FadeState.FadeStart:
+					fadeState = FadeState.FadeIn;
+					break;
+
+				case FadeState.FadeIn:
+					skySituation.text = SkyNames[0];
+					if (fadeState == FadeState.FadeIn) FadeIn();
+					break;
+				case FadeState.FadeOut:
+					if (fadeState == FadeState.FadeOut) FadeOut();
+					break;
+			}
 		}
 
 	}
@@ -91,16 +182,18 @@ public class SkySystem : MonoBehaviour
 		//	skySituation.color = skySituation.color + new Color32(0, 0, 0, 1);
 		//	new WaitForSeconds(0.5f);
 		//}
+		
 		alpha += 0.001f;
 		skySituation.color = new Color(0, 0, 0, alpha);
 		if (alpha >= 1)
 		{
 			nextWaitTime += Time.deltaTime;
-			if (nextWaitTime >= 3.0f)
+			if (nextWaitTime >= 2.0f)
 			{
 				nextWaitTime = 0;
-				fadeIn = false;
-				fadeOut = true;
+				//fadeIn = false;
+				//fadeOut = true;
+				fadeState = FadeState.FadeOut;
 			}
 
 		}
@@ -113,12 +206,14 @@ public class SkySystem : MonoBehaviour
 		//	skySituation.color = skySituation.color - new Color32(0, 0, 0, 1);
 		//	new WaitForSeconds(0.1f);
 		//}
+		
 		alpha -= 0.001f;
 		skySituation.color = new Color(0, 0, 0, alpha);
 		if (alpha <= 0)
 		{
-			fadeOut = false;
 
+			//fadeOut = false;
+			fadeState = FadeState.FadeEnd;
 		}
 	}
 }
