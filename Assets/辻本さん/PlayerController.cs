9a80c590f012075dbf3732e5963e9c56b3bb1f11
bs_work-admin <hmj2422019@stu.o-hara.ac.jp>
2026-02-02 06:11:06 +0900

    一応登り切れることを確認済み

--- Assets/辻本さん/PlayerController.cs
+++ Assets/辻本さん/PlayerController.cs
@@ -6,296 +6,301 @@ using UnityEngine.InputSystem.XR;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-	public enum State
-	{
-		Normal,
-		Ghost
-	}
+    public enum State
+    {
+        Normal,
+        Ghost
+    }
 
-	[Header("基本移動設定")]
-	[SerializeField] private float speed = 3f;
-	[SerializeField] private float dashSpeed = 6f;
-	[SerializeField] private float dashAcceleration = 10f;
+    [Header("基本移動設定")]
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float dashSpeed = 6f;
+    [SerializeField] private float dashAcceleration = 10f;
 
-	[Header("ジャンプ設定")]
-	[SerializeField] private float jumpSpeed = 7f;
+    [Header("ジャンプ設定")]
+    [SerializeField] private float jumpSpeed = 7f;
 
-	[Header("重力設定")]
-	[SerializeField] private float gravity = 15f;
-	[SerializeField] private float fallSpeed = 10f;
-	[SerializeField] private float initFallSpeed = 2f;
+    [Header("重力設定")]
+    [SerializeField] private float gravity = 15f;
+    [SerializeField] private float fallSpeed = 10f;
+    [SerializeField] private float initFallSpeed = 2f;
 
-	[Header("カメラ設定")]
-	[SerializeField] private Camera targetCamera;
+    [Header("カメラ設定")]
+    [SerializeField] private Camera targetCamera;
 
-	[Header("壁登り設定")]
-	[SerializeField] private float climbSpeed = 3f;
-	[SerializeField] private float wallCheckDistance = 0.6f;
+    [Header("壁登り設定")]
+    [SerializeField] private float climbSpeed = 1f;
+    [SerializeField] private float wallCheckDistance = 0.6f;
 
-	[Header("壁判定")]
-	[SerializeField] private float wallSphereRadius = 0.4f;
-
-
-	[Header("ゴースト設定")]
-	[SerializeField] private float ghostSpeed = 5f;
-	[SerializeField] private float ghostVerticalSpeed = 3f;
-
-	private Transform _transform;
-	private CharacterController characterController;
-	private Vector2 inputMove;
-	private float verticalVelocity;
-	private float turnVelocity;
-	private bool isGroundedPrev;
-	private bool isClimbing;
-
-	private bool isDashing;
-	private float currentSpeed;
-
-	private RaycastHit wallHit;
-	private State currentState = State.Normal;
-
-	// アニメーション
-	private Animator animator;
-
-	// 入力
-	public void OnMove(InputAction.CallbackContext context)
-	{
-
-		inputMove = context.ReadValue<Vector2>();
-	}
-
-	public void OnJump(InputAction.CallbackContext context)
-	{
-		if (currentState == State.Ghost) return; // ゴースト中はジャンプ禁止
-		if (!context.performed) return;
-		if (!characterController.isGrounded) return;
-
-		//if (!context.performed || !characterController.isGrounded) return;
-		verticalVelocity = jumpSpeed;
-		animator.SetTrigger("Jump");
-		//Debug.Log("OnJump");
-	}
-
-	private void Awake()
-	{
-		_transform = transform;
-		characterController = GetComponent<CharacterController>();
-		animator = GetComponent<Animator>();
-		if (targetCamera == null)
-			targetCamera = Camera.main;
-
-		currentSpeed = speed;
-	}
-
-	private void Update()
-	{
-		if (!gameObject.activeInHierarchy || !characterController.enabled)
-			return;
-
-		switch (currentState)
-		{
-			case State.Normal:
-				UpdateNormal();
-				break;
-
-			case State.Ghost:
-				UpdateGhost();
-				break;
-		}
-
-		UpdateAnimator();
-	}
+    [Header("壁判定")]
+    [SerializeField] private float wallSphereRadius = 0.4f;
 
 
-	private void UpdateAnimator()
-	{
-		Vector3 horizontalVelocity = characterController.velocity;
-		horizontalVelocity.y = 0f;
+    [Header("ゴースト設定")]
+    [SerializeField] private float ghostSpeed = 5f;
+    [SerializeField] private float ghostVerticalSpeed = 3f;
 
-		// 移動量
-		animator.SetFloat("Speed", horizontalVelocity.magnitude);
+    private Transform _transform;
+    private CharacterController characterController;
+    private Vector2 inputMove;
+    private float verticalVelocity;
+    private float turnVelocity;
+    private bool isGroundedPrev;
+    private bool isClimbing;
 
-		// 接地
-		animator.SetBool("IsGrounded", characterController.isGrounded);
+    private bool isDashing;
+    private float currentSpeed;
 
-		// 落下
-		animator.SetFloat("yVelocity", verticalVelocity);
+    private RaycastHit wallHit;
+    private State currentState = State.Normal;
 
-		// 壁登り
-		animator.SetBool("IsClimbing", isClimbing);
-		animator.SetFloat("ClimbSpeed", Mathf.Abs(inputMove.y));
-	}
-	// 通常
-	private void UpdateNormal()
-	{
-		bool isGrounded = characterController.isGrounded;
-		bool isTouchingWall = false;
+    // アニメーション
+    private Animator animator;
 
-		float verticalInput = inputMove.y;
-		bool hasClimbInput = Mathf.Abs(verticalInput) > 0.1f;
+    // 入力
+    public void OnMove(InputAction.CallbackContext context)
+    {
 
-		// SphereCastの始点を頭付近に変更
-		Vector3 start = _transform.position + Vector3.up * (characterController.height * 0.8f);
+        inputMove = context.ReadValue<Vector2>();
+    }
 
-		if (Physics.SphereCast(start, wallSphereRadius, _transform.forward, out wallHit, wallCheckDistance))
-		{
-			float wallDot = Vector3.Dot(wallHit.normal, Vector3.up);
-			if (wallDot < 0.5f)
-				isTouchingWall = true;
-		}
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (currentState == State.Ghost) return; // ゴースト中はジャンプ禁止
+        if (!context.performed) return;
+        if (!characterController.isGrounded) return;
 
-		// 壁登り処理
-		if (isTouchingWall && Input.GetMouseButton(0) && hasClimbInput)
-		{
-			isClimbing = true;
-			verticalVelocity = 0f;
+        //if (!context.performed || !characterController.isGrounded) return;
+        verticalVelocity = jumpSpeed;
+        animator.SetTrigger("Jump");
+        //Debug.Log("OnJump");
+    }
 
-			// 壁法線取得
-			if (Physics.SphereCast(start, wallSphereRadius, _transform.forward, out wallHit, wallCheckDistance))
-			{
-				Vector3 wallNormal = wallHit.normal;
+    private void Awake()
+    {
+        _transform = transform;
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        if (targetCamera == null)
+            targetCamera = Camera.main;
 
-				// 上方向成分も含めた登る方向
-				Vector3 climbDir = Vector3.Cross(Vector3.Cross(wallNormal, Vector3.up), wallNormal).normalized;
-				Vector3 move = climbDir * (verticalInput * climbSpeed) + Vector3.up * (verticalInput * climbSpeed)
-							   - wallNormal * 0.2f;
+        currentSpeed = speed;
+    }
 
-				characterController.Move(move * Time.deltaTime);
+    private void Update()
+    {
+        if (!gameObject.activeInHierarchy || !characterController.enabled)
+            return;
 
-				// 壁向き回転
-				Quaternion targetRot = Quaternion.LookRotation(-wallNormal);
-				_transform.rotation = Quaternion.Slerp(_transform.rotation, targetRot, Time.deltaTime * 20f);
-			}
+        switch (currentState)
+        {
+            case State.Normal:
+                UpdateNormal();
+                break;
 
-			// 壁登り中に登りきったかチェック
-			if (CheckClimbOver(wallHit))
-			{
-				isClimbing = false;
-				verticalVelocity = jumpSpeed * 0.5f; // 少し押し上げて自然に着地
-			}
+            case State.Ghost:
+                UpdateGhost();
+                break;
+        }
 
-			return;
-		}
-		else
-		{
-			isClimbing = false;
-		}
-
-		// ダッシュ処理
-		if (Keyboard.current.leftShiftKey.isPressed && !isClimbing && isGrounded)
-			isDashing = true;
-		else
-			isDashing = false;
-
-		float targetSpeed = isDashing ? dashSpeed : speed;
-		currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * dashAcceleration);
-
-		// 重力処理
-		if (!isClimbing)
-		{
-			if (isGrounded && !isGroundedPrev)
-				verticalVelocity = -initFallSpeed;
-			else if (!isGrounded)
-			{
-				verticalVelocity -= gravity * Time.deltaTime;
-				if (verticalVelocity < -fallSpeed)
-					verticalVelocity = -fallSpeed;
-			}
-		}
-		isGroundedPrev = isGrounded;
-
-		// 移動
-		float cameraAngleY = targetCamera.transform.eulerAngles.y;
-		Vector3 moveVelocity = new Vector3(inputMove.x * currentSpeed, verticalVelocity, inputMove.y * currentSpeed);
-		moveVelocity = Quaternion.Euler(0, cameraAngleY, 0) * moveVelocity;
-		characterController.Move(moveVelocity * Time.deltaTime);
-
-		// 向き
-		if (inputMove != Vector2.zero)
-		{
-			float targetAngleY = -Mathf.Atan2(inputMove.y, inputMove.x) * Mathf.Rad2Deg + 90f;
-			targetAngleY += cameraAngleY;
-			float angleY = Mathf.SmoothDampAngle(_transform.eulerAngles.y, targetAngleY, ref turnVelocity, 0.1f);
-			_transform.rotation = Quaternion.Euler(0, angleY, 0);
-		}
-	}
+        UpdateAnimator();
+    }
 
 
-	// ゴースト
-	private void UpdateGhost()
-	{
-		Vector3 move = new Vector3(inputMove.x, 0, inputMove.y);
+    private void UpdateAnimator()
+    {
+        Vector3 horizontalVelocity = characterController.velocity;
+        horizontalVelocity.y = 0f;
 
-		// カメラ基準で移動方向を変換
-		float cameraY = targetCamera.transform.eulerAngles.y;
-		move = Quaternion.Euler(0, cameraY, 0) * move;
-		move.Normalize();
+        // 移動量
+        animator.SetFloat("Speed", horizontalVelocity.magnitude);
 
-		// 上下移動
-		float upDown = 0f;
-		if (Keyboard.current.spaceKey.isPressed)
-			upDown += 1f;
-		if (Keyboard.current.leftCtrlKey.isPressed)
-			upDown -= 1f;
+        // 接地
+        animator.SetBool("IsGrounded", characterController.isGrounded);
 
-		move.y = upDown * ghostVerticalSpeed;
+        // 落下
+        animator.SetFloat("yVelocity", verticalVelocity);
 
-		// 実際の移動
-		characterController.Move(move * ghostSpeed * Time.deltaTime);
-	}
+        // 壁登り
+        animator.SetBool("IsClimbing", isClimbing);
+        animator.SetFloat("ClimbSpeed", Mathf.Abs(inputMove.y));
+    }
+    // 通常
+    private void UpdateNormal()
+    {
+        bool isGrounded = characterController.isGrounded;
+        bool isTouchingWall = false;
 
-	private bool CheckClimbOver(RaycastHit wallHit)
-	{
-		Vector3 headPos = _transform.position + Vector3.up * (characterController.height * 0.5f);
-		if (Physics.Raycast(headPos, Vector3.up, out RaycastHit upHit, 0.8f))
-		{
-			return false;
-		}
+        float verticalInput = inputMove.y;
+        bool hasClimbInput = Mathf.Abs(verticalInput) > 0.1f;
 
-		Vector3 forwardPos = _transform.position + (-wallHit.normal * 0.3f) + Vector3.up * 1.0f;
-		if (Physics.Raycast(forwardPos, Vector3.down, out RaycastHit groundHit, 1.5f))
-		{
-			if (Vector3.Dot(groundHit.normal, Vector3.up) > 0.8f)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
+        // SphereCastの始点を頭付近に変更
+        Vector3 start = _transform.position + Vector3.up * (characterController.height * 0.5f);
 
-	// 状態切り替え
-	public void SetState(State newState)
-	{
-		currentState = newState;
-		//Debug.Log($"State changed to: {currentState}");
-	}
+        if (Physics.SphereCast(start, wallSphereRadius, _transform.forward, out wallHit, wallCheckDistance))
+        {
+            float wallDot = Vector3.Dot(wallHit.normal, Vector3.up);
+            if (wallDot < 0.5f)
+                isTouchingWall = true;
+        }
 
-	public State GetState()
-	{
-		return currentState;
-	}
+        // 壁登り処理
+        if (isTouchingWall && Input.GetMouseButton(0) && hasClimbInput)
+        {
+            isClimbing = true;
+            verticalVelocity = 0f;
 
-	private void OnDrawGizmosSelected()
-	{
-		if (_transform == null) _transform = transform;
-		if (characterController == null) characterController = GetComponent<CharacterController>();
+            // 壁法線取得
+            if (Physics.SphereCast(start, wallSphereRadius, _transform.forward, out wallHit, wallCheckDistance))
+            {
+                Vector3 wallNormal = wallHit.normal;
 
-		float sphereRadius = wallSphereRadius;
-		float distance = wallCheckDistance;
+                // 上方向も含めた登る方向
+                Vector3 climbDir = Vector3.Cross(Vector3.Cross(wallNormal, Vector3.up), wallNormal).normalized;
+                //Vector3 move = climbDir * (verticalInput * climbSpeed) + Vector3.up * (verticalInput * climbSpeed)- wallNormal * 0.2f;
 
-		Vector3 start = _transform.position + Vector3.up * (characterController.height * 0.5f);
-		Vector3 end = start + _transform.forward * distance;
+                Vector3 move = Vector3.up * (verticalInput * climbSpeed) - wallNormal * 0.3f;
+                characterController.Move(move * Time.deltaTime);
 
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawWireSphere(start, sphereRadius); // 始点
-		Gizmos.color = Color.cyan;
-		Gizmos.DrawWireSphere(end, sphereRadius);   // 終点
-		Gizmos.color = Color.white;
-		Gizmos.DrawLine(start, end);                // 線
+                // 壁向き回転
+                Quaternion targetRot = Quaternion.LookRotation(-wallNormal);
+                _transform.rotation = Quaternion.Slerp(_transform.rotation, targetRot, Time.deltaTime * 20f);
+            }
 
-		// 登りきりチェック位置のデバック
-		Gizmos.color = Color.green;
-		Vector3 headPos = _transform.position + Vector3.up * (characterController.height * 0.5f);
-		Gizmos.DrawWireSphere(headPos, 0.1f);
-	}
+            // 壁登り中に登りきったかチェック
+            if (CheckClimbOver(wallHit))
+            {
+                isClimbing = false;
+                verticalVelocity = jumpSpeed * 0.5f; // 少し押し上げて自然に着地
+            }
+
+            return;
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        // ダッシュ処理
+        if (Keyboard.current.leftShiftKey.isPressed && !isClimbing && isGrounded)
+            isDashing = true;
+        else
+            isDashing = false;
+
+        float targetSpeed = isDashing ? dashSpeed : speed;
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * dashAcceleration);
+
+        // 重力処理
+        if (!isClimbing)
+        {
+            if (isGrounded && !isGroundedPrev)
+                verticalVelocity = -initFallSpeed;
+            else if (!isGrounded)
+            {
+                verticalVelocity -= gravity * Time.deltaTime;
+                if (verticalVelocity < -fallSpeed)
+                    verticalVelocity = -fallSpeed;
+            }
+        }
+        isGroundedPrev = isGrounded;
+
+        // 移動
+        float cameraAngleY = targetCamera.transform.eulerAngles.y;
+        Vector3 moveVelocity = new Vector3(inputMove.x * currentSpeed, verticalVelocity, inputMove.y * currentSpeed);
+        moveVelocity = Quaternion.Euler(0, cameraAngleY, 0) * moveVelocity;
+        characterController.Move(moveVelocity * Time.deltaTime);
+
+        // 向き
+        if (inputMove != Vector2.zero)
+        {
+            float targetAngleY = -Mathf.Atan2(inputMove.y, inputMove.x) * Mathf.Rad2Deg + 90f;
+            targetAngleY += cameraAngleY;
+            float angleY = Mathf.SmoothDampAngle(_transform.eulerAngles.y, targetAngleY, ref turnVelocity, 0.1f);
+            _transform.rotation = Quaternion.Euler(0, angleY, 0);
+        }
+    }
+
+
+    // ゴースト
+    private void UpdateGhost()
+    {
+        Vector3 move = new Vector3(inputMove.x, 0, inputMove.y);
+
+        // カメラ基準で移動方向を変換
+        float cameraY = targetCamera.transform.eulerAngles.y;
+        move = Quaternion.Euler(0, cameraY, 0) * move;
+        move.Normalize();
+
+        // 上下移動
+        float upDown = 0f;
+        if (Keyboard.current.spaceKey.isPressed)
+            upDown += 1f;
+        if (Keyboard.current.leftCtrlKey.isPressed)
+            upDown -= 1f;
+
+        move.y = upDown * ghostVerticalSpeed;
+
+        // 実際の移動
+        characterController.Move(move * ghostSpeed * Time.deltaTime);
+    }
+
+    private bool CheckClimbOver(RaycastHit wallHit)
+    {
+        Vector3 headPos = _transform.position + Vector3.up * (characterController.height - 0.1f);
+        if (Physics.Raycast(headPos, Vector3.up, 0.5f))
+        {
+            return false;
+        }
+
+        Vector3 forwardPos = _transform.position + (-wallHit.normal * 0.2f) + Vector3.up * (characterController.height * 0.5f);
+        if (Physics.Raycast(forwardPos, Vector3.down, out RaycastHit groundHit, characterController.height))
+        {
+            if (Vector3.Dot(groundHit.normal, Vector3.up) > 0.7f)
+            {
+                return true;
+
+            }
+        }
+
+        return false;
+    }
+
+    // 状態切り替え
+    public void SetState(State newState)
+    {
+        currentState = newState;
+        //Debug.Log($"State changed to: {currentState}");
+    }
+
+    public State GetState()
+    {
+        return currentState;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_transform == null) _transform = transform;
+        if (characterController == null) characterController = GetComponent<CharacterController>();
+
+        Vector3 start = _transform.position
+            + Vector3.up * (characterController.height * 0.8f); // ★合わせる
+
+        Vector3 end = start + _transform.forward * wallCheckDistance;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(start, wallSphereRadius);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(end, wallSphereRadius);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(start, end);
+
+        if (Physics.SphereCast(start, wallSphereRadius, _transform.forward, out wallHit, wallCheckDistance))
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(wallHit.point, wallHit.normal);
+        }
+    }
+
 }
